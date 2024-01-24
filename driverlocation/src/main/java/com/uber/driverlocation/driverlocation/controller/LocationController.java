86b92b17d62c0b35a670cf0d027d11098b4f2ddc
Sumegh from Acer <samytembhurne@gmail.com>
2024-01-24 10:29:15 +0530

    updating Kafka Producer and Consumer App

--- driverlocation/src/main/java/com/uber/driverlocation/driverlocation/controller/LocationController.java
+++ driverlocation/src/main/java/com/uber/driverlocation/driverlocation/controller/LocationController.java
@@ -1,6 +1,9 @@
 package com.uber.driverlocation.driverlocation.controller;
 
 import com.uber.driverlocation.driverlocation.service.KafkaMessagingService;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -12,13 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/location")
 public class LocationController {
-
+    Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
     @Autowired
     KafkaMessagingService kafkaMessagingService;
 
     @PostMapping("/update")
     public ResponseEntity<String> updateLocation(){
         for (int i =0; i < 100000; i++){
+            LOGGER.info("produced updated driver location for instance {}", i);
             kafkaMessagingService.updateLocation("("+Math.random()*100+", "+Math.random()*100+")");
         }
         return new ResponseEntity<>("driver location updated", HttpStatus.OK);
